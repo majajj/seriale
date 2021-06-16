86dd5b3ac7385a38185dd76630f362c320efbb37
Maria Sagan <maja_jj@interia.pl>
2021-06-16 22:19:12 +0200

    search show function is working

---
+++ shows/config.py
@@ -0,0 +1,6 @@
+title_id = None
+
+shows_url = 'http://api.tvmaze.com/schedule/web?date=2020-01-07'
+
+actors_url = 'http://api.tvmaze.com/shows/'
+actors_ending_url = '/cast'
---
+++ shows/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from .views import \
+    search_show, \
+    search_actor_from_show
+
+urlpatterns = [
+    path('search-shows/', search_show),
+    path('<str:title_id>/actors', search_actor_from_show)
+    
+]
+
+
